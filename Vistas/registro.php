fa9b5b08eac7e68aefcc0ed6ab768f14d2e09297
NanoMO-1972 <fernandomazarro@hotmail.com>
2025-03-19 20:01:06 +0100

    he cambioado cosas

--- Vistas/registro.php
+++ Vistas/registro.php
@@ -6,7 +6,7 @@
     <title>Restaurante Don Giovanni</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../Assets/scriptsValidacionFormulario.js"></script>
-    <link rel="stylesheet" href="CSS/Style.css">
+    
      <!-- link css de Bootsrap -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- link iconos de Bootsrap -->
@@ -26,26 +26,42 @@
 
 <?php 
     include 'Includes/header.html';
-?>
-  
-  <section id="altaUsuario">
-        
-        <h2 class="tituloSeccion">Alta de Usuarios</h2>
+?> 
+          
+<h2 class="tituloSeccion text-center mb-4">Registro</h2>
 
-            <form action="" id="formularioAltaUsuario">
-
-                <input type="text" id="nombre" name="Nombre" placeholder="Nombre y Apellidos" required>
-                <input type="email" id="email" name="email" placeholder="email" required>
-                <input type="tel" id="telefono" name="telefono" placeholder="Teléfono Ej: +34 600 123 456" required>
-                <input type="password" id="password" name="password" placeholder="password" required>
-                <input type="password" id="confirmarClave" name="confirmarClave" placeholder="confirmarClave" required="">                
-                <input id="botonEnviar" type="submit" name="submit" placeholder="Enviar">
-
-            </form>
-
+<form action="" id="formularioAltaUsuario" class="container mt-4 p-4 border rounded bg-light shadow">
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="nombre" class="form-label">Nombre y Apellidos</label>
+            <input type="text" id="nombre" name="Nombre" class="form-control" placeholder="Nombre y Apellidos" autocomplete="off" required>
         </div>
-
-    </section>
+        
+        <div class="col-md-6 mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" id="email" name="email" class="form-control" placeholder="email" autocomplete="off" required>
+        </div>
+    </div>
+    
+    <div class="mb-3">
+        <label for="telefono" class="form-label">Teléfono</label>
+        <input type="tel" id="telefono" name="telefono" class="form-control" placeholder="Teléfono Ej: +34 600 123 456" autocomplete="off" required>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="password" class="form-label">Contraseña</label>
+            <input type="password" id="password" name="password" class="form-control" placeholder="Contraseña" autocomplete="off" required>
+        </div>
+        
+        <div class="col-md-6 mb-3">
+            <label for="confirmarClave" class="form-label">Confirmar Contraseña</label>
+            <input type="password" id="confirmarClave" name="confirmarClave" class="form-control" placeholder="Confirmar contraseña" autocomplete="off" required>
+        </div>
+    </div>
+    
+    <button id="botonEnviar" type="submit" class="btn btn-primary w-100">Enviar</button>
+</form>
 
     <?php 
     
