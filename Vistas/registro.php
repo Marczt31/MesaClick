b42b5e1438c226fe73f45da5ac7b5dcb2004678a
Marc <marczt2002@gmail.com>
2025-04-09 20:32:13 +0200

    Link iconos corregido

--- Vistas/registro.php
+++ Vistas/registro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="Assets/imagenes/cuchillos_blanco.png" />
+    <link rel="shortcut icon" href="../Assets/imagenes/cuchillos_blanco.png" />
     <title>MesaClick</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../Assets/scriptsValidacionFormulario.js"></script> 
