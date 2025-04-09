a475e986a7d08ca1a131464f5724b22ebf281f9a
Marc <marczt2002@gmail.com>
2025-04-09 20:28:46 +0200

    titulo e icono de pagina login y registro

--- Vistas/login.php
+++ Vistas/login.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurante Don Giovanni</title>
+    <link rel="shortcut icon" href="Assets/imagenes/cuchillos_blanco.png" />
+    <title>MesaClick</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="../Assets/scriptsValidacionFormulario.js"></script> 
         
