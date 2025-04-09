8cdad89934a3a84554fcd85bea3062b51e10c976
Marc <marczt2002@gmail.com>
2025-04-09 20:35:14 +0200

    header y footer añadidos a registroSatisfactorio

--- Vistas/registroSatisfactorio.php
+++ Vistas/registroSatisfactorio.php
@@ -5,7 +5,19 @@
     <title>Registro exitoso</title>
 </head>
 <body>
+<?php 
+
+    include 'Includes/header.html';
+
+?> 
+
     <h2>¡Registro completado con éxito!</h2>
     <a href="index.php">Volver al inicio</a>
+
+<?php 
+    
+    include 'Includes/footer.html';
+
+?>
 </body>
 </html>
